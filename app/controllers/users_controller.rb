ce52f698207259136cc194b2b43565c951e16e35
Cyberia 2045 <sagansociety86@gmail.com>
2017-03-02 19:04:28 -0500

    updated my styling and layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:first_name, :email, :password, :avatar)
+		params.require(:user).permit(:first_name, :email, :password, :avatar, :bio)
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302185809) do
+ActiveRecord::Schema.define(version: 20170302225624) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170302185809) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "bio"
   end
 
 end
